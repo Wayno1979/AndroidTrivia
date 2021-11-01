b7a5ac6dc9f914754da6c24a3babbf6fbb54a678
Wayno1979 <waynecockram@gmail.com>
2021-11-01 17:06:06 +0000

    updated to use navigation created onclick listener to navigate

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -18,11 +18,9 @@ class TitleFragment : Fragment() {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_title, container, false)
 
-        binding.playButton.setOnClickListener{ view: View ->
-            // find the instance of the navigation controller
-            // navigate to the fragment using navigation.xml created id
-            Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
-        }
+        binding.playButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+        )
         return binding.root
     }
 }
