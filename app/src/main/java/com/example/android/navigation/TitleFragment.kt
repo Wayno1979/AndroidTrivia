6e9209ed5a2aed7dabd450de1f95d8177874b628
Wayno1979 <waynecockram@gmail.com>
2021-11-01 17:00:17 +0000

    Added navigation graph, navigates to relevant fragment

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,14 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
 
     override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?,
                                savedInstanceState: Bundle?): View? {
+
+        // initialise binding for the view and inflate fragment
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_title, container, false)
+
+        binding.playButton.setOnClickListener{ view: View ->
+            // find the instance of the navigation controller
+            // navigate to the fragment using navigation.xml created id
+            Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
+        }
         return binding.root
     }
 }
