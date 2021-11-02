3da809c1428677561796afde0942d95c2c1d64ed
Wayno1979 <waynecockram@gmail.com>
2021-11-02 19:02:39 +0000

    added a navdrawer and allowed navigation to relevant fragments

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -21,11 +21,13 @@ import android.os.Bundle
 import android.view.*
 import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentGameWonBinding
 
 class GameWonFragment : Fragment() {
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
